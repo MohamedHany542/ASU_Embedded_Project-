6920420a97aea0782104cf1c844125d6a53263ad
mahmoudhussein25 <89795768+mahmoudhussein25@users.noreply.github.com>
2024-05-02 20:26:56 +0300

    Delete LedSwitchInitH.h

--- LedSwitchInitH.h
+++
@@ -1,7 +0,0 @@
-#define switch_mask  0x10
-#define GREEN_LED_mask    0x08
-void switch1_int();
-void LED_Green_int();
-unsigned char read_switch1();
-void GREEN_LED_out (unsigned char LED_in );
-void GREEN_LED_clear (void);
